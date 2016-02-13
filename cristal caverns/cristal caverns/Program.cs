2b8e9387463886b8a3280818bdd33e93e9853e83
Tmq5521 <Tommy.queenan@gmail.com>
2016-02-13 14:08:01 -0700

    Fixed problems Relaeasde more code

--- cristal caverns/cristal caverns/Program.cs
+++ cristal caverns/cristal caverns/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// day 1 2 hours
+// day 2 2:15 hours
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +16,11 @@ namespace cristal_caverns
     class cavern //object template for each room
     {
         public sbyte[] pos; // position in array
-        public string discribe, exits; //strings containing discriptions of the rooms and exits
+        public string discribe; // strings containing discription of the room
+        public string[] exits; //strings containing discriptions of the exits
         public string[][,,] paths ; //array containing the path each exit takes
 
-        public cavern(sbyte[] pos, string discribe, string exits, string[][,,] paths )
+        public cavern(sbyte[] pos, string discribe, string[] exits, string[][,,] paths )
         {
             // sets variables in the new object
             this.pos = pos;
@@ -37,6 +43,8 @@ namespace cristal_caverns
     }
     class Program
     {
+        static object[,,] Map1 = new object[100,100,100];
+
         static void Load() // loads map from file
         {
             string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "save.txt"); // stores current directory in path
@@ -44,22 +52,55 @@ namespace cristal_caverns
             {
                 if (line.Contains("Cavern:")) // finds all cavern defs from file
                 {
-                    string temp = line.Substring(8); // looses the "Cavern:" part of the string
-                    int i = 8; 
-                    for (int I = 1; I <= temp.Length; I++) // iterates through the line
+                    byte B = 0;
+                    sbyte[] C;
+                    C = new sbyte[3];
+                    string T = line.Substring(8); // looses the "Cavern:" part of the string
+                    int i = 0;
+                    for (int I = 1; I < T.Length; I++) // iterates through the line
                     {
-                        if (temp[I] == ","[0] ) // finds commas
+                        if (T[I] == ","[0]) // finds commas
                         {
-                            string test = (temp.Substring(i, (i - I)));
-                            int.Parse(test); //pointer
+
+                            string test = (T.Substring(i, (I - i)));
+                            Console.WriteLine(test);
+                            Console.WriteLine("{0}", i);
+                            C[B] = sbyte.Parse(test); //pointer
+
+                            i = I + 1;
+                            B++;
+                        }
+                    }
+
+                    string[] E; // test junk
+                    string[][,,] D;
+                    E = new string[6];
+                    D = new string[6][,,]; // ...
+
+                    cavern Cavern = new cavern(C, "", E, D); //make new room
+
+                    Map1[ C[0], C[1], C[2]] = Cavern; // store into a array
+
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Load();
+            for(byte x = 0; x < 100; x++)
+            {
+                for(byte y = 0; y < 100; y++)
+                {
+                    for(byte z = 0; z < 100; z++)
+                    {
+                        if (Map1[x, y, z] != null)
+                        {
+                            Console.WriteLine(Map1[x, y, z]);
                         }
                     }
                 }
             }
-        }
-        static void Main(string[] args)
-        {
-            Load();
             Console.ReadKey(); // pause...
         }
     }
